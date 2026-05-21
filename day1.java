a6385364950cd4b81ebfe5d67658e892665ce102
aathi531 <146916288+aathi531@users.noreply.github.com>
2026-05-21 22:22:43 +0530

    Create day1.java with data type examples
    data types

---
+++ day1.java
@@ -0,0 +1,32 @@
+public class day1 {
+    //main method 
+    public static void main(String[] args) {
+        //data types
+        String a="aathi";
+        char b='a';
+        
+        float c=5.80F;
+        double d=2345.90;
+        long e=123456789L;
+        short f=234;
+        byte g=56;
+        int  i=33;
+
+         //printing statement
+        System.out.println(a);
+        System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(i);
+
+
+
+
+
+    }
+     
+
+}
